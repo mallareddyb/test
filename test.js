14342c355238dc55bd6c8fe4f2f535189cf7e327
mallareddyb <36248675+mallareddyb@users.noreply.github.com>
2018-02-07 16:38:48 -0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,21 @@
+// Copyright (C) 2014 Sony Mobile Communications Inc.
+// All rights, including trade secret rights, reserved.
+
+/*jshint node:true */
+'use strict';
+
+
+var expect = require('chai').expect;
+var x = require('../abc.xml');
+
+
+
+    describe('#getUrl()', function () {
+        it(abc, function () {
+            var a=2;
+            var b=2;
+            expect(a).to.equal(2);
+        });
+    });
+
+    
