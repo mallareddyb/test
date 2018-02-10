bf6628a8919a37f3c1e762cff247ae7ea5467d2b
mallareddyb <36248675+mallareddyb@users.noreply.github.com>
2018-02-09 16:31:55 -0800

    Update test.js

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ var x = require('../abc.xml');
         it(abc, function () {
             
             
-            var c=343;
+            var c=43;
             var a=2;
             var b=2;
             expect(a).to.equal(2);
