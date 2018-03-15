552b091f41f222716853af21c927c214bca9c8ee
mallareddyb <36248675+mallareddyb@users.noreply.github.com>
2018-03-14 17:15:10 -0700

    Update test.js

--- test.js
+++ test.js
@@ -4,11 +4,10 @@
 /*jshint node:true */
 'use strict';
 
-
 var expect = require('chai').expect;
     describe('#getUrl()', function () {
         it('abc', function () {   
-            var c=330;
+            var c=3300;
             var a=2;
             var b=2;
             expect(a).to.equal(2);
