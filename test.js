31ee77f82bc0fc1fae951b5ef388f189c0c69d30
mallareddyb <36248675+mallareddyb@users.noreply.github.com>
2018-07-18 15:49:18 -0700

    Update test.js

--- test.js
+++ test.js
@@ -7,13 +7,10 @@
 
 var expect = require('chai').expect;
 
-
-
     describe('#getUrl()', function () {
         it('abc', function () {
             
-            
-            var c=304;
+            var c=3;
             var a=2;
             var b=2;
             expect(a).to.equal(2);
